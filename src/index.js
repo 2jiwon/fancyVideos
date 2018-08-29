4795d5110923e99cc02204c5e425f3ea87d36e56
2jiwon <ljwjulian@gmail.com>
2018-08-29 14:30:49 +0900

    Made a new 'SearchBar' component based on class.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyA_5ujA5HcA7dXsOHEOTYckqhGJSEhtI3w';
+
 // Create a new component. This component should produce some HTMl.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this components generated HTML and put it on the page (in the DOM).
