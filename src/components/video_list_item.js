5991e8416edee5f4b45c5edbeab2e8fc11f80ff6
2jiwon <ljwjulian@gmail.com>
2018-09-01 18:47:21 +0900

    Prepare the video list as use bootstrap.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
   return (
-    <li></li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading"></div>
+        </div>
+      </div>
+    </li>
   );
 };
 
