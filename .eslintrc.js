e43d3e23993852b3c89a02d78c0c3f7118681846
2jiwon <ljwjulian@gmail.com>
2018-09-02 01:45:37 +0900

    Modified eslint settings little.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,45 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "settings": {
+    "escmascript": 6,
+    "jsx": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      "space"
     ],
-    "rules": {
-        "indent": [
-            "error",
-          "space"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-          "always", {
-            "omitLastInOneLineBlock": true 
-          },
-        ]
-    }
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always", {
+        "omitLastInOneLineBlock": true
+      },
+    ]
+  }
 };
