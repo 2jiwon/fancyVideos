2510bc5b37649c8921e8213a09a4403193bece44
2jiwon <ljwjulian@gmail.com>
2018-09-09 15:48:26 +0900

    Added a method into the SearchBar.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+
+    this.videoSearch('surfboards');
   }
 
   videoSearch (term) {
@@ -30,7 +32,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
